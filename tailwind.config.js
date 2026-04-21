60161ea4afc7f22778fa8e1fbf0af95249a6019a
francyrivera43-sudo <francyrivera43@gmail.com>
2026-04-21 16:58:25 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+/** @type {import('tailwindcss').Config} */ export default { content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], theme: { extend: { colors: { "primary": "#0058be", "background": "#f8f9ff", "surface": "#f8f9ff" }, }, }, plugins: [], }
